25b837713e5b2a900cb6695e3bf2f5276b9e31b3
seid1zahidov <saidiz@code.edu.az>
2022-03-28 13:49:57 -0700

    homework

---
+++ script.js
@@ -0,0 +1,29 @@
+//let name1 = "seid"
+//let name2 = "ilkin"
+//let name3 = "huseyn"
+
+let names = ["huseyn","eli","cavid","huseyn","ilkin"]
+names[5] = "murad";
+
+let name = names[3];
+
+
+
+console.log(names[3]);
+console.log(name)
+console.log(names.length)
+console.log(names)
+
+//for (let index = 0; index < names.length; index++) {
+  //  console.log(names[index])
+
+//}
+
+let student = 
+{
+    name : "ilkin",
+    surname:"zahidov",
+    age : 24
+}
+
+console.log(student.name+" "+student.surname+" "+student.age)
